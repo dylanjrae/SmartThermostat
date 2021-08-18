240072b820394e236560935c3030b967ba6f2439
Dylan Rae <55325297+djohnjames@users.noreply.github.com>
2021-08-17 21:35:32 -0600

    Fixing code error.

--- WebDev/app.py
+++ WebDev/app.py
@@ -377,7 +377,7 @@ def handle_connect(client, userdata, flags, rc):
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
-    return
+    # return
     message.payload = message.payload.decode("utf-8")
     # print("Received message '" + str(message.payload) + "' on topic '"
     #     + message.topic + "' with QoS " + str(message.qos))
